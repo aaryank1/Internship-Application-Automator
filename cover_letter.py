7a00bb0c44c0d300f8d780162fb8e404092c2881
Aaryan <aaryankakade1@gmail.com>
2024-11-16 22:04:39 +0530

    captcha bypass and resume upload

--- cover_letter.py
+++ cover_letter.py
@@ -17,4 +17,5 @@ def generate_cover_letter(internship):
 	prompt = "Write a cover letter for the above internship opportunity. Use relevant parts of the attached resume. Keep it concise. Use at most 2035 characters. The cover letter should not contain places where the user/applicant needs to make modifications. It should be ready to submit in the application form"
 	internship = internship + '\n\n' + prompt
 	response = model.generate_content([internship, pdf])
+	print(response.text)
 	return response.text
--- internshala.py
+++ internshala.py
@@ -12,6 +12,7 @@ email = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
 internshala_url = os.getenv("INTERNSHALA_URL")
 
+
 class InternshalaAutomation(JobAutomation):
 	def login(self):
 		self.driver.get("https://internshala.com/")
@@ -21,7 +22,15 @@ class InternshalaAutomation(JobAutomation):
 
 		email_input = self.driver.find_element(By.NAME, value='email')
 		pwd_input = self.driver.find_element(By.NAME, value='password')
+
+		# The random sleep() and clicks are used to mimic human behaviour to bypass captcha
+		time.sleep(2)
+		pwd_input.click()
+		time.sleep(1)
+		email_input.click()
 		email_input.send_keys(self.username)
+		time.sleep(3)
+		pwd_input.click()
 		pwd_input.send_keys(self.password)
 
 		login_btn = self.driver.find_element(by=By.ID, value='modal_login_submit')
@@ -29,6 +38,7 @@ class InternshalaAutomation(JobAutomation):
 		time.sleep(3)
 
 	def fill_form(self, internship):
+		# Check and fill Cover letter
 		try:
 			time.sleep(1)
 			cover_letter_container = self.driver.find_element(by=By.CSS_SELECTOR, value="#cover_letter_holder .ql-editor")
@@ -39,6 +49,22 @@ class InternshalaAutomation(JobAutomation):
 			print("No cover letter input box found")
 			pass
 
+		# Resume Upload
+		resume = os.path.abspath(os.path.join(os.path.dirname(__file__), "assets", "Resume.pdf"))
+		file_input = self.driver.find_element(by=By.CSS_SELECTOR, value="input[type='file']")
+		file_input.send_keys(resume)
+		time.sleep(5)
+
+		# Laptop and Internet availability
+		try:
+			laptop_internet_question = self.driver.find_element(by=By.XPATH, value="//label[contains(text(), 'Do you have a working laptop and internet?')]")
+			yes_option = self.driver.find_element(by=By.XPATH, value="//input[@type='radio' and @value='Yes']")
+			yes_option.click()
+		except:
+			print("No laptop question or other error")
+			pass
+
+
 	def find_jobs_and_apply(self):
 		self.driver.get(internshala_url)
 		list_of_jobs = self.driver.find_elements(by=By.CSS_SELECTOR, value='#internship_list_container_1 .visibilityTrackerItem')
@@ -46,10 +72,13 @@ class InternshalaAutomation(JobAutomation):
 		job_links = []
 		for job in list_of_jobs:
 			try:
-				# print(job.find_element(by=By.CSS_SELECTOR, value="div div div h3").text)
-				job_links.append(job.get_attribute("id"))
+				job_title = job.find_element(by=By.CSS_SELECTOR, value="div div div h3").text
+				if "WordPress" not in job_title and "Nextjs" not in job_title and "PHP" not in job_title:
+					print(job_title)
+					job_links.append(job.get_attribute("id"))
 			except:
 				continue
+		print(job_links)
 
 		# print(job_links)
 		original_window = self.driver.current_window_handle
